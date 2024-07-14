5a7ab67e41fed3c43c4f96a03b94243fc1067a1d
carbeeno <wilsonko289@gmail.com>
2024-07-14 13:40:09 +0100

    modified config for serve and build flexibility

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,26 @@ import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl';
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), glsl()],
-  base: '/ghibli-grass-v2/'
-  // base: command === 'build' ? '/ghibli-grass-v2/' : '/',
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), glsl()],
+//   base: '/ghibli-grass-v2/'
+//   // base: command === 'build' ? '/ghibli-grass-v2/' : '/',
+// })
+
+export default defineConfig(({ command }) => {
+  
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      plugins: [react(), glsl()]
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      plugins: [react(), glsl()],
+      base: '/ghibli-grass-v2/'
+    }
+  }
 })
